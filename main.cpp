1054fc41424ed2981473f82f1812dfa521bd4018
Bigofficial <1439635048@qq.com>
2024-07-29 10:53:12 +0800

    修改为快拍算法

--- main.cpp
+++ main.cpp
@@ -4,23 +4,31 @@
 //张毅来噜
 
 using namespace std;
-void sort(int[] arr, int size){
-	int temp = 0;
-	bool flag = false;
-	for(int i = 0; i < size - 1; i++){
-	for(int j = 0; j < size - 1; j++){
-		//小张改的<
-		if(arr[j] < arr[i])
-		{
-
-			int temp = j;
-			j = i;
-			i = temp;
-			flag = true;
+void sort(int arr[], int i, int j){
+	int val = arr[i];
+	int l = i;
+	int r = j;
+	while(l < j){
+		while(l < r && arr[r] > val){
+			r--;
 		}
-		if(!flag) break;
+		if(l < r){
+			arr[l] = arr[r];
+			l++;
+		}
+		while(l<r && arr[l] <= val){
+			l++;
+		}
+		if(l < r){
+			arr[r--] = arr[l];
+		}
 	}
-	}
+	arr[l] = val;
+	sort(arr, i, l-1);
+	sort(arr, l+ 1, j);
+}
+void sort(int[] arr, int size){
+	sort(arr, 0, size);
 }
 int main(){
 	
